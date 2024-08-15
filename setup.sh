8fb0a49f6e917c7069cb328304c375c399733ee3
FlorinN <florin01@tpg.com.au>
2024-08-15 21:28:20 +1000

    azure details

--- setup.sh
+++ setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="testml"
-LOCATION="eastus"
-WORKSPACE="azureml"
+GROUP="fnmlops"
+LOCATION="Australia East"
+WORKSPACE="fnmlops"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
